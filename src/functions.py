e46361bc999fcc51c6d10f140954130c65b15d8a
Raman Auseyenka <avseenko00@mail.ru>
2022-02-17 20:35:09 +0300

    fix

--- src/functions.py
+++ src/functions.py
@@ -9,12 +9,11 @@ def euklid(a, mod):
     t0 = gcd_ext(a, mod)[0]
     if t0 < 0:
         t0 += mod
-    return t0, a, mod
+    return t0
 
 
 def quick_pow(base, pow_decimal, mod):
     pow_bin = []
-    _pow = pow_decimal
     while pow_decimal:
         pow_bin.append(pow_decimal % 2)
         pow_decimal //= 2
@@ -25,12 +24,12 @@ def quick_pow(base, pow_decimal, mod):
         temp = pow(temp, 2) % mod
         if i == 1:
             temp = (temp * base) % mod
-    return temp, base, _pow, mod
+    return temp
 
 
 def find_roots(c, mod):
     a_pow2 = c % mod
-    a1 = quick_pow(a_pow2, (mod + 1) / 4, mod)[0]
+    a1 = quick_pow(a_pow2, (mod + 1) / 4, mod)
     a2 = mod - a1
     return a1, a2
 
@@ -39,8 +38,8 @@ def calculate(p, q, c):
     m = p * q
     m1 = q
     m2 = p
-    m1_rev = euklid(m1, p)[0]
-    m2_rev = euklid(m2, q)[0]
+    m1_rev = euklid(m1, p)
+    m2_rev = euklid(m2, q)
 
     x12 = find_roots(c, p)
     x34 = find_roots(c, q)
